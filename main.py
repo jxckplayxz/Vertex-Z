3fdfe56ac9444861d2c8a10282d05c9ef4e539a3
jxckplayxz <156136126+jxckplayxz@users.noreply.github.com>
2025-11-18 05:17:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3444,7 +3444,9 @@ def serve_encrypted_keys():
 
 
 def run_flask():
-    app.run(host="0.0.0.0", port=5080)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sets $PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
 
 
 if __name__ == "__main__":
